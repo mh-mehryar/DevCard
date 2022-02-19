9d9089a31089bd7914a4e6d4a93596af1476f0ab
Mohammad Hossein <mh.mehryar@gmail.com>
2022-02-19 13:21:06 +0330

    projects and latestarticles moved to viewcomponent

---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1," تاکسی","پروژه درخواست تاکسی آنلاین","project-1.jpg","Tap30"),
+                new Project(2," زودفود","پروژه درخواست آنلاین غذا","project-2.jpg","ZoodFood"),
+                new Project(3," مدارس","سیستم مدیریت یکپارچه مدارس","project-3.jpg","Monop"),
+                new Project(4," پت شاپ","فروشگاه آنلاین فروش محصولات حیوانات خانگی","project-4.jpg","DogShop"),
+            };
+            return View("_Projects",projects);
+        }
+    }
+}
